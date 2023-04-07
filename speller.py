577d46f9740fb76df41ef3e116ad6cd7070a0c2a
Levente Székely <31960469+player1s@users.noreply.github.com>
2023-04-07 17:55:47 +0200

    refined main menu, put choices in different files, started speller class

---
+++ speller.py
@@ -0,0 +1,10 @@
+# here the idea is that we make a speller object which has a pre defined alphabet to work on, then also make functions 
+# to navigate within that. 
+
+class Speller():
+    def __init__(self):
+        alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+
+        
